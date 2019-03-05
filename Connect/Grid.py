82672b1c8de5d964438b89cf19a97346476099ea
josejesusgarciajr <josejesusgarciajr@gmail.com>
2019-03-04 21:23:30 -0800

    Validation of column full or not fixed, still needs validation for lenght of the matrix

--- Connect/Grid.py
+++ Connect/Grid.py
@@ -6,6 +6,7 @@ Created on Mar 4, 2019
 
 from Player import Player
 from inspect import stack
+from __builtin__ import True
    
 class Grid:
     def __init__(self, player1, player2, matrix):
@@ -15,10 +16,20 @@ class Grid:
         
     def addMove(self, column, player):
         row = len(self.matrix[column]) - 1
-        print("LENGHT OF COLUMN: " + str(row))
-        while(self.matrix[column][row] != 0):
-            row -= 1
-        self.matrix[column][row] = player.color
+
+        (notFull, row) = self.columnNotFull(column)
+        
+        if notFull:
+            self.matrix[column][row] = player.color # if Not FUll (ADD TO COLUMN)
+        else:
+            notValidEntry = True
+            while notValidEntry:
+                column = input(player.name + " choose a column that is not full: ") - 1
+                validEntry, row = self.columnNotFull(column)
+
+                if validEntry:
+                    self.matrix[column][row] = player.color
+                    notValidEntry = False
         printGame(self.matrix)
     
     def whosTurn(self, player1, player2):
@@ -28,6 +39,14 @@ class Grid:
         else:
             col = input("Which Column Player 2?")
             return (col - 1), player2, player1
+        
+    def columnNotFull(self, column):
+        row = len(self.matrix[column]) - 1
+        while (self.matrix[column][row] != 1 or self.matrix[column][row]) != 2 and row >= 0:
+            if self.matrix[column][row] == 0:
+                return (True, row)
+            row -= 1
+        return (False , -1)
  
 def printGame(game):
     for row in game:
